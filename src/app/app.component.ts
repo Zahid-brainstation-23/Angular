581b291f06dfc3121ddb2b868807f36681250fef
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-08-07 21:44:00 +0600

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {BrowserModule} from "@angular/platform-browser";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
+  standalone: true,
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
 })
 export class AppComponent {
   title = 'Angular';
