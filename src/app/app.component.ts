f9191a12378321560542958cc49f27bca6764c42
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-08-08 11:52:27 +0600

    customized the deatails page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {BrowserModule} from "@angular/platform-browser";
 import {HomeComponent} from "./home/home.component";
 
 @Component({
